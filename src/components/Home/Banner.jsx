0c470d1c5799fa805dc4e7b8834ab384fe1ab921
MD Hasim <mdhasim.nana.2001@gmail.com>
2024-11-20 23:31:35 +0600

    client site setup

---
+++ src/components/Home/Banner.jsx
@@ -0,0 +1,8 @@
+
+export const Banner = () => {
+  return (
+    <div>
+        <img src="https://opencart4.magentech.com/themes/so_xtore/layout3/image/catalog/banners/about-us.jpg" alt="" />
+    </div>
+  )
+}
